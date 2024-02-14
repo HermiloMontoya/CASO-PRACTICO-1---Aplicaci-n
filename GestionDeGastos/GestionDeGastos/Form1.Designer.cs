22240d0d5e0df9416021bae599f8ec2ee103cbc1
HermiloMontoya <143041208+HermiloMontoya@users.noreply.github.com>
2024-02-14 13:52:37 -0600

    Add files via upload

---
+++ GestionDeGastos/GestionDeGastos/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace GestionDeGastos
+{
+    partial class FPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mostrarBaseDeDatos = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombreGasto = new System.Windows.Forms.TextBox();
+            this.txtTotalGasto = new System.Windows.Forms.TextBox();
+            this.txtFechaAgregar = new System.Windows.Forms.DateTimePicker();
+            this.txtFechaMostrar = new System.Windows.Forms.DateTimePicker();
+            this.btnTodo = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbRegistros = new System.Windows.Forms.Label();
+            this.lbGastos = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.mostrarBaseDeDatos)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del gasto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(440, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total del gasto:";
+            // 
+            // mostrarBaseDeDatos
+            // 
+            this.mostrarBaseDeDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mostrarBaseDeDatos.Location = new System.Drawing.Point(43, 279);
+            this.mostrarBaseDeDatos.Name = "mostrarBaseDeDatos";
+            this.mostrarBaseDeDatos.Size = new System.Drawing.Size(534, 206);
+            this.mostrarBaseDeDatos.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fecha:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnAgregar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.ForeColor = System.Drawing.Color.Black;
+            this.btnAgregar.Location = new System.Drawing.Point(232, 108);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(149, 35);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnMostrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.btnMostrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.btnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrar.ForeColor = System.Drawing.Color.Black;
+            this.btnMostrar.Location = new System.Drawing.Point(386, 229);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(103, 35);
+            this.btnMostrar.TabIndex = 5;
+            this.btnMostrar.Text = "MOSTRAR";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("NSimSun", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(167, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(267, 27);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "GESTIÓN DE GASTOS";
+            // 
+            // txtNombreGasto
+            // 
+            this.txtNombreGasto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreGasto.Location = new System.Drawing.Point(171, 54);
+            this.txtNombreGasto.MaxLength = 30;
+            this.txtNombreGasto.Name = "txtNombreGasto";
+            this.txtNombreGasto.Size = new System.Drawing.Size(242, 29);
+            this.txtNombreGasto.TabIndex = 7;
+            // 
+            // txtTotalGasto
+            // 
+            this.txtTotalGasto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalGasto.Location = new System.Drawing.Point(445, 54);
+            this.txtTotalGasto.MaxLength = 10;
+            this.txtTotalGasto.Name = "txtTotalGasto";
+            this.txtTotalGasto.Size = new System.Drawing.Size(155, 29);
+            this.txtTotalGasto.TabIndex = 8;
+            this.txtTotalGasto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTotalGasto_KeyPress);
+            // 
+            // txtFechaAgregar
+            // 
+            this.txtFechaAgregar.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaAgregar.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtFechaAgregar.Location = new System.Drawing.Point(25, 57);
+            this.txtFechaAgregar.Name = "txtFechaAgregar";
+            this.txtFechaAgregar.Size = new System.Drawing.Size(115, 26);
+            this.txtFechaAgregar.TabIndex = 9;
+            // 
+            // txtFechaMostrar
+            // 
+            this.txtFechaMostrar.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFechaMostrar.Location = new System.Drawing.Point(43, 234);
+            this.txtFechaMostrar.Name = "txtFechaMostrar";
+            this.txtFechaMostrar.Size = new System.Drawing.Size(322, 26);
+            this.txtFechaMostrar.TabIndex = 10;
+            // 
+            // btnTodo
+            // 
+            this.btnTodo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnTodo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnTodo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnTodo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTodo.ForeColor = System.Drawing.Color.Black;
+            this.btnTodo.Location = new System.Drawing.Point(495, 229);
+            this.btnTodo.Name = "btnTodo";
+            this.btnTodo.Size = new System.Drawing.Size(82, 35);
+            this.btnTodo.TabIndex = 11;
+            this.btnTodo.Text = "TODO";
+            this.btnTodo.UseVisualStyleBackColor = false;
+            this.btnTodo.Click += new System.EventHandler(this.btnTodo_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(105, 503);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(249, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "TOTAL DE REGISTROS:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(92, 545);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(262, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "TOTAL DE LOS GASTOS:";
+            // 
+            // lbRegistros
+            // 
+            this.lbRegistros.AutoSize = true;
+            this.lbRegistros.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRegistros.Location = new System.Drawing.Point(360, 497);
+            this.lbRegistros.Name = "lbRegistros";
+            this.lbRegistros.Size = new System.Drawing.Size(34, 31);
+            this.lbRegistros.TabIndex = 14;
+            this.lbRegistros.Text = "--";
+            // 
+            // lbGastos
+            // 
+            this.lbGastos.AutoSize = true;
+            this.lbGastos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGastos.Location = new System.Drawing.Point(360, 540);
+            this.lbGastos.Name = "lbGastos";
+            this.lbGastos.Size = new System.Drawing.Size(34, 31);
+            this.lbGastos.TabIndex = 15;
+            this.lbGastos.Text = "--";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(631, 48);
+            this.panel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.txtFechaAgregar);
+            this.panel2.Controls.Add(this.txtTotalGasto);
+            this.panel2.Controls.Add(this.txtNombreGasto);
+            this.panel2.Controls.Add(this.btnAgregar);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(631, 159);
+            this.panel2.TabIndex = 17;
+            // 
+            // FPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(631, 593);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbGastos);
+            this.Controls.Add(this.lbRegistros);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTodo);
+            this.Controls.Add(this.txtFechaMostrar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.mostrarBaseDeDatos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GestionDeGastos";
+            ((System.ComponentModel.ISupportInitialize)(this.mostrarBaseDeDatos)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView mostrarBaseDeDatos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombreGasto;
+        private System.Windows.Forms.TextBox txtTotalGasto;
+        private System.Windows.Forms.DateTimePicker txtFechaAgregar;
+        private System.Windows.Forms.DateTimePicker txtFechaMostrar;
+        private System.Windows.Forms.Button btnTodo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbRegistros;
+        private System.Windows.Forms.Label lbGastos;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ GestionDeGastos/GestionDeGastos/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionDeGastos
+{
+    public partial class FPrincipal : Form
+    {
+
+        public class Gasto
+        {
+            public DateTime Fecha { get; set; }
+            public string NombreDelGasto { get; set; }
+            public decimal TotalDelGasto { get; set; }
+        }
+
+        private List<Gasto> listaGastos = new List<Gasto>();
+
+
+        public FPrincipal()
+        {
+            InitializeComponent();
+
+            mostrarBaseDeDatos.DataSource = listaGastos;
+
+            mostrarBaseDeDatos.Columns[0].Width = 160; // Fecha
+            mostrarBaseDeDatos.Columns[1].Width = 200; // Nombre
+            mostrarBaseDeDatos.Columns[2].Width = 130; // Total
+        }
+
+        //UTILIZAMOS UN METODO DE CODIGO ASCII PARA QUE SOLO SE PERMITA CARACTERES NUMERICOS PARA NUMERO ENTEROS O DECIMALES.
+        private void SoloNumeros(KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 45 || (e.KeyChar >= 58 && e.KeyChar <= 255)))
+            {
+                MessageBox.Show("Solo se permite caracteres numericos y caracter de '.' para decimal.", "Caractere invalido", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtTotalGasto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            SoloNumeros(e);
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+           
+            if (string.IsNullOrWhiteSpace(txtNombreGasto.Text) || string.IsNullOrWhiteSpace(txtTotalGasto.Text))
+            {
+                MessageBox.Show("ERROR: Ingrese el nombre y el total del gasto.");
+                return;
+            }
+
+          
+            if (!decimal.TryParse(txtTotalGasto.Text, out decimal total))
+            {
+                MessageBox.Show("ERROR: Ingrese un total de gasto válido.");
+                return;
+            }
+
+           
+            DateTime fecha = txtFechaAgregar.Value;
+
+           
+            Gasto nuevoGasto = new Gasto
+            {
+                Fecha = fecha,
+                NombreDelGasto = txtNombreGasto.Text,
+                TotalDelGasto = total
+            };
+
+            
+            listaGastos.Add(nuevoGasto);
+
+            LimpiarTxt();
+
+            ActualizarDataGridView();
+            calcularTodo(listaGastos);
+        }
+
+        private void LimpiarTxt()
+        {
+            txtNombreGasto.Clear();
+            txtTotalGasto.Clear();
+        }
+
+        private void ActualizarDataGridView()
+        {     
+            mostrarBaseDeDatos.DataSource = null; 
+            mostrarBaseDeDatos.DataSource = listaGastos;
+
+            mostrarBaseDeDatos.DataSource = listaGastos;
+
+            mostrarBaseDeDatos.Columns[0].Width = 160; // Fecha
+            mostrarBaseDeDatos.Columns[1].Width = 200; // Nombre
+            mostrarBaseDeDatos.Columns[2].Width = 130; // Total
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            DateTime fechaBusqueda = txtFechaMostrar.Value.Date;
+
+            List<Gasto> gastosFiltrados = listaGastos.FindAll(g => g.Fecha.Date == fechaBusqueda);
+
+            mostrarBaseDeDatos.DataSource = gastosFiltrados;
+
+            calcularSelecion(); //Metodo que calcula el gasto por fecha de selecion de los registros.
+        }
+
+        private void btnTodo_Click(object sender, EventArgs e)
+        {
+            ActualizarDataGridView();
+            calcularTodo(listaGastos); //Metodo que calcula el gasto de todos los registros.
+            
+        }
+
+        private void calcularTodo(List<Gasto> gastos)
+        {
+           
+            decimal total = gastos.Sum(g => g.TotalDelGasto);
+            lbGastos.Text = total.ToString();
+
+            int registros = mostrarBaseDeDatos.RowCount;
+            lbRegistros.Text = registros.ToString();
+        }
+
+        private void calcularSelecion()
+        {
+            decimal total = 0;
+
+            if (mostrarBaseDeDatos.Rows.Count > 0)
+            {
+                foreach (DataGridViewRow row in mostrarBaseDeDatos.Rows)
+                {
+                    Gasto gasto = (Gasto)row.DataBoundItem;
+
+                    total += gasto.TotalDelGasto;
+                }
+            }
+
+            int registros = mostrarBaseDeDatos.RowCount;
+            lbRegistros.Text = registros.ToString();
+            lbGastos.Text = total.ToString();
+        }
+    }
+}
